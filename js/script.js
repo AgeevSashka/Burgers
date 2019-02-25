378bbf03b2f4441367662bc09357d3b08eb9f32a
Alexandr Ageev <kingno04061994@gmail.com>
2019-02-25 22:09:23 +0300

    Реализовал два аккордиона , форму и слайдер

--- js/script.js
+++ js/script.js
@@ -20,3 +20,304 @@ btn.addEventListener('click', function(e){
 
     
 });
+
+// Аккардеон Команда 
+  const items = document.querySelectorAll(".team__item");
+
+  for (item of items) {
+  
+    item.addEventListener("click", e => {
+      e.preventDefault();
+      const curItem = e.currentTarget;
+  
+      if (curItem.classList.contains('acco-active')) {
+        curItem.classList.remove('acco-active');
+      } else {
+  
+        Array.from(items).forEach(elem => {
+          elem.classList.remove('acco-active');
+        })
+  
+        curItem.classList.add('acco-active');
+      }
+  
+    });
+  }
+// Горизонтальный акко
+
+const accoMenuList = document.querySelector('.menu-accordeon__list');
+const accoMenuItem = document.querySelectorAll('.menu-accordeon__item');
+let accoMenuItemLength = accoMenuItem.length;
+const accoMenuClose = document.querySelector('.menu-accordeon__close');
+
+accoMenuList.addEventListener('click', function (e) {
+    for (let i = 0; i < accoMenuItemLength; i++) {
+        accoMenuItem[i].classList.remove('menu-accordeon__item-active');
+    }
+});
+
+for (let i = 0; i < accoMenuItemLength; i++) {
+    accoMenuItem[i].addEventListener('click', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (accoMenuItem[i].classList.contains('menu-accordeon__item-active')) {
+            accoMenuItem[i].classList.remove('menu-accordeon__item-active');
+        } else {
+            for (let i = 0; i < accoMenuItemLength; i++) {
+                accoMenuItem[i].classList.remove('menu-accordeon__item-active');
+            }
+            accoMenuItem[i].classList.add('menu-accordeon__item-active');
+        }
+    });
+};
+
+accoMenuClose.addEventListener('click', function (e) {
+    accoMenuItem.classList.remove('menu-accordeon__item-active');
+});
+
+
+// Модалка
+const reviewsList = document.querySelector('.reviews');
+const popupReviews = document.querySelector('.popup__reviews');
+const popupTitle = document.querySelector('.popup__content-title');
+const popupText = document.querySelector('.popup__content-text');
+const closePopupRev = document.querySelector('.popup__close-reviews');
+const overlay = document.querySelector('.overlay');
+
+reviewsList.addEventListener('click', e => {
+    let elementRev = e.target;
+    var _body = document.getElementsByTagName('body')[0];
+    _body.style.overflow = "hidden";
+
+    if (elementRev.tagName === 'A') {
+        e.preventDefault();
+        const reviewsItem = e.target.closest('.reviews__block');
+        const reviewsTitle = reviewsItem.querySelector('.reviews__title').textContent;
+        const reviewsText = reviewsItem.querySelector('.reviews__subtitle').textContent;
+        popupTitle.innerHTML = reviewsTitle;
+        popupText.innerHTML = reviewsText;
+        popupReviews.style.display = 'block';
+    }
+});
+
+//Слайдер
+var leftArrow = document.querySelector('.arrow-left');
+var rightArrow = document.querySelector('.arrow-right');
+
+var slides = document.querySelectorAll('.main__slider-content .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+leftArrow.addEventListener('click', () =>{
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide+1)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+});
+
+rightArrow.addEventListener('click', () =>{
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+});
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+
+// Форма с модалкой 
+
+const myForm = document.querySelector('#form');
+  const orderBtn = myForm.querySelector('.order__form-button');
+  const clearBtn = myForm.querySelector('.order__form-button-reset');
+  const formTrue = myForm.querySelector('.formoverlay-true');
+  const formErr = myForm.querySelector('.formoverlay-error');
+  const formtruecloseBtn = myForm.querySelector('.formtrue__close');
+  const formerrcloseBtn = myForm.querySelector('.formerr__close');
+  
+  orderBtn.addEventListener('click', e=> {
+    event.preventDefault();
+    if (validateForm(myForm)) {
+        const name = myForm.elements.name.value;
+        const phone = myForm.elements.phone.value;
+        const comment = myForm.elements.comment.value;
+        const to = 'webdev@mail.ru';
+        var formData = new FormData();
+            formData.append('name', name);
+            formData.append('phone', phone);
+            formData.append('comment', comment);
+            formData.append('to', to);
+            console.log(formData);
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'json';
+            xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail/');
+            xhr.send(formData);
+            xhr.addEventListener('load', e => {
+                if (xhr.response.status) {
+                  formTrue.style.display = 'block';    
+                } else {
+                  formErr.style.display = 'block';
+                }
+
+                formtruecloseBtn.addEventListener('click', function() {
+                  event.preventDefault();
+                  formTrue.style.display = 'none';
+                })
+
+                formTrue.addEventListener('click', function() {
+                  event.preventDefault();
+                  formTrue.style.display = 'none';
+                })
+
+                document.addEventListener('keyup', e => {
+                  let keyName = e.keyCode;
+                
+                  if (keyName === 27) {
+                    formTrue.style.display = 'none';
+                  }
+                })
+
+                formerrcloseBtn.addEventListener('click', function() {
+                  event.preventDefault();
+                  formErr.style.display = 'none';
+                })
+
+                formErr.addEventListener('click', function() {
+                  event.preventDefault();
+                  formErr.style.display = 'none';
+                })
+                
+                document.addEventListener('keyup', e => {
+                  let keyName = e.keyCode;
+                
+                  if (keyName === 27) {
+                    formErr.style.display = 'none';
+                  }
+                })
+                
+            })
+    }
+})
+
+function validateForm(myForm) {
+  let valid = true;
+  
+  if (!validateField(myForm.elements.name)) {
+      valid = false;
+  }
+
+  if (!validateField(myForm.elements.phone)) {
+      valid = false;
+  }
+
+  if (!validateField(myForm.elements.comment)) {
+      valid = false;
+  }
+
+  if (!validateField(myForm.elements.street)) {
+    valid = false;
+  }
+
+  if (!validateField(myForm.elements.home)) {
+    valid = false;
+  }
+
+  return valid;
+}
+
+function validateField(field) {
+  if (!field.checkValidity()) {
+      field.nextElementSibling.textContent = field.validationMessage;
+      return false;
+  }
+  else {
+      field.nextElementSibling.textContent = '';
+      return true;
+  }
+}
+
+const nameForm = document.querySelector('#name');
+const phoneForm = document.querySelector('#phone');
+const homeForm = document.querySelector('#home');
+const floorForm = document.querySelector('#floor');
+
+nameForm.addEventListener('keydown', function(event) {
+  let isDigit = true;
+  let isDash = false;
+  let isControl = false;
+
+  if (event.key >= 0 || event.key <=9) {
+    isDigit = false;
+  }
+
+  if (event.key == '-') {
+    isDash = true;
+  }
+
+  if (event.key == 'ArrowLeft' || event.key =='ArrowRight' ||event.key == 'Backspace') {
+    isControl = true;
+  }
+
+  if (isDigit == false && isDash == false && isControl == false) {
+    event.preventDefault();
+  }
+})
+
+phoneForm.addEventListener('keydown', function(event) {
+  let isDigit = false;
+  let isDash = false;
+  let isControl = false;
+
+  if (event.key >= 0 || event.key <=9) {
+    isDigit = true;
+  }
+
+  if (event.key == '-') {
+    isDash = true;
+  }
+
+  if (event.key == 'ArrowLeft' || event.key =='ArrowRight' ||event.key == 'Backspace') {
+    isControl = true;
+  }
+
+  if (isDigit == false && isDash == false && isControl == false) {
+    event.preventDefault();
+  }
+})
+
+homeForm.addEventListener('keydown', function(event) {
+  let isDigit = false;
+  let isControl = false;
+
+  if (event.key >= 0 || event.key <=9) {
+    isDigit = true;
+  }
+
+  if (event.key == 'ArrowLeft' || event.key =='ArrowRight' ||event.key == 'Backspace') {
+    isControl = true;
+  }
+
+  if (isDigit == false && isControl == false) {
+    event.preventDefault();
+  }
+})
+
+floorForm.addEventListener('keydown', function(event) {
+  let isDigit = false;
+  let isControl = false;
+
+  if (event.key >= 0 || event.key <=9) {
+    isDigit = true;
+  }
+
+  if (event.key == 'ArrowLeft' || event.key =='ArrowRight' ||event.key == 'Backspace') {
+    isControl = true;
+  }
+
+  if (isDigit == false && isControl == false) {
+    event.preventDefault();
+  }
+})
